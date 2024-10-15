ec0c9dbc227dd182ea9f12bb2fb678b756eaedeb
Takkino31 <takkinoyaya@gmail.com>
2024-10-15 21:39:37 +0000

    Les Controllers 006

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+    protected $primaryKey  = 'id';
+    protected $fillable = [
+        'title',
+        'slug'
+    ];
 }
--- routes/web.php
+++ routes/web.php
@@ -19,29 +19,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::prefix('/blog')->name('blog.')->group(function () {
+Route::prefix('/blog')->name('blog.')->controller(\App\Http\Controllers\BlogController::class)->group(function () {
 
-    Route::get('/', function () {
-//        $post = new Post();
-//        $post->title = 'Mon troisieme article';
-//        $post->slug = 'mon-troisieme-article';
-//        $post->content = 'Mon contenu de troisieme article';
-//        $post->save();
-        return App\Models\Post::all();
-//        dd(App\Models\Post::all());
+    Route::get('/', 'index')->name('index');
 
-        return [
-            'link' =>\route('blog.show', ['slug' => 'article', 'id' => 13]),
-        ];
-    })->name('index');
-
-    Route::get('/{slug}-{id}', function (string $slug,string $id, Request $request) {
-        return [
-            'slug' => $slug,
-            'id' => $id,
-            'name' => $request->input('name'),
-        ];
-    })->where([
+    Route::get('/{slug}-{id}', 'show')->where([
         'id' => '[0-9]+',
         'slug' => '[a-z0-9\-]+',
     ])->name('show');
